420c64b325c5348e0c787248fee6b67646515f2d
Imtua <itsololevelling@gmail.com>
2022-12-15 13:23:16 +0300

    Добавьте файлы проекта.

---
+++ Skilfactory_3.3.2/Program.cs
@@ -0,0 +1,16 @@
+﻿string myName;
+sbyte age;
+bool isAnimal;
+double footSize;
+
+myName = "Vladimir";
+age = 21;
+isAnimal = true;
+footSize = 44;
+
+Console.WriteLine("My name is: {0}", myName);
+Console.WriteLine("My age is: {0}", age);
+Console.WriteLine("Do I have a pet? {0}", isAnimal);
+Console.WriteLine("My shoe size is: {0}", footSize);
+
+Console.ReadKey();
